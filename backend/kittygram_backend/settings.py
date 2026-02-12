d44559cfaf468b86556aeac74820d2148d8835a8
Tatiana <tmalakh@yahoo.com>
2026-02-12 10:41:39 +0300

    Fixed minor things

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -105,6 +105,7 @@ MEDIA_ROOT = '/app/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated', 
