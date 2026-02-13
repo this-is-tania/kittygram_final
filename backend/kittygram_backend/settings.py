f6b807af47f2384f4fb4e51c05df3a77dfeece3b
Tatiana <tmalakh@yahoo.com>
2026-02-13 11:45:47 +0300

    Fixed minor things

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,17 +1,18 @@
 # flake8: noqa
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY", 'data_is_hidden')
+SECRET_KEY = os.getenv('SECRET_KEY', 'data_is_hidden')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
